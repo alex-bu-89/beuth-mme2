32121eae159fb7c608f2ca799cd7a0afe806a346
Alexander Buyanov <s54160@beuth-hochschule.de>
2015-11-03 18:15:22 +0100

    task 2 is ready

--- u2/app.js
+++ u2/app.js
@@ -1,12 +1,47 @@
 var express = require('express');
+var fs = require('fs')
 var app = express();
 
-// loads static files from public folder
+// Aufgabe 3 loads static files from public folder
 app.use('/public', express.static(__dirname + '/public'));
 
-// Route anlegen
-app.get('/*', function (req, res) {
-    //console.log(res);
+// Aufgabe 4
+app.get('/time', function (req, res) {
+    var date = new Date();
+    var current_time =  date.getDate() + "/" +
+                        (date.getMonth()+1)  + "/" +
+                        date.getFullYear() + " | " +
+                        date.getHours() + ":" +
+                        date.getMinutes() + ":" +
+                        date.getSeconds();
+    res.header("Content-Type", "text/plain");
+    res.send("the time is: " + current_time + "\n" +"content-type is: " + res.get('Content-Type'));
+});
+
+// Aufgabe 5a
+app.get('/file', function (req, res) {
+
+    var path = "./file/file.txt";
+    var data = "It's a new filefile new new  new file";
+    var start, end;
+
+   /* fs.writeFile(path, data, { flags: 'wx' }, function (err) {
+        if (err) throw err;
+        console.log("File created");
+    });*/
+
+    start = process.hrtime(); // start time
+
+    var content = fs.readFile(path, 'utf-8', function(err, data) {
+        if (err) throw err;
+        end = process.hrtime(start); // execution time
+        res.send("execution time: " + end + "ms" + "<br /><br />" + data);
+    });
+
+});
+
+// Aufgabe 1 route anlegen
+app.get('*', function (req, res) {
     res.send('<!DOCTYPE html>' +
         '<html lang="de">' +
         '<head><meta charset="utf-8"></head>' +
